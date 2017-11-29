45e8770a9a825d17f5811aef0a6ac50eaba21690
Maria Thomas <malethr@gmail.com>
2017-11-28 16:05:46 -0800

    add initial style

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,10 @@ import { Text, View } from 'react-native';
 
 // Make a registerComponent
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
   return (
-    <View>
+    <View style={viewStyle}>
       <Text style={textStyle}>Albums!</Text>
     </View>
   );
@@ -15,10 +15,19 @@ const Header = () => {
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
-    fontSize: 100
+    fontSize: 20
   }
 };
 
