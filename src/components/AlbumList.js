3c55a1eaa230cf2bac59acf39fa1336f14cca8da
Maria Thomas <malethr@gmail.com>
2017-11-29 10:57:52 -0800

    add album detail and album list js file

---
+++ src/components/AlbumList.js
@@ -0,0 +1,31 @@
+import React, { Component }from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail'
+
+class AlbumList extends Component {
+state = { albums: [] };
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => response.json())
+    .then((responseData) => this.setState({ albums: responseData }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title}>{album.title}</AlbumDetail>);
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
